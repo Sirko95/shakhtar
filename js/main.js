da7a062119b7511f399c2155c845b968beb691a1
Oleg Sirota <olegsirota95@gmail.com>
2016-05-17 14:36:46 +0300

    begin

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var app = angular.module('app', ['ngRoute']);
+
+app.config(function ($routeProvider) {
+   $routeProvider
+       .when('/',{
+           templateUrl: 'home.html'
+       })
+       .when('/player',{
+           templateUrl: 'player.html'
+       })
+});
